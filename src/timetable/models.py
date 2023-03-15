940a50cd46cb72f5e39cc369cf1d1967e6d7c55d
Stanislav <stas-chubar@yandex.ru>
2023-03-15 13:04:08 +0700

    minor update for timetable models in src/timetable/models.py

--- src/timetable/models.py
+++ src/timetable/models.py
@@ -8,9 +8,13 @@ class Teachers(models.Model):
     """
     # Columns
     id = models.IntegerField(primary_key=True)
-    familiya = models.CharField()
-    imya = models.CharField()
-    otchestvo = models.CharField()
+    familiya = models.CharField(max_length=50, help_text="Фамилия")
+    imya = models.CharField(max_length=50, help_text="Имя")
+    otchestvo = models.CharField(max_length=50, help_text="Отчество")
+
+    class Meta:
+        verbose_name = "Teacher"
+        ordering = ["familiya", "imya", "otchestvo"]
 
     def __str__(self):
         return f"Teacher {self.id}> {self.familiya} {self.imya} {self.otchestvo}"
@@ -22,9 +26,13 @@ class Lessons(models.Model):
     """
     # Columns
     id = models.IntegerField(primary_key=True)
-    title = models.CharField()
-    room = models.TextField()
-    teacher = models.ForeignKey(Teachers, on_delete=models.CASCADE())
+    title = models.CharField(max_length=100, help_text="Название предмета")
+    room = models.TextField(max_length=100, help_text="Номер аудитории")
+    teacher = models.ForeignKey(Teachers, on_delete=models.CASCADE(), help_text="ФИО преподавателя")
+
+    class Meta:
+        verbose_name = "Lesson"
+        ordering = ["title"]
 
     def __str__(self):
         return f"Lesson {self.id}> {self.title} | {self.room} | {self.teacher}"
