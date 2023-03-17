062b8627ebe27ee34afffb6492a69cf7eb6abdbf
Shatan69 <Shpilberg.n@yandex.ru>
2023-03-17 18:20:14 +0700

    added new models

--- src/chat/models.py
+++ src/chat/models.py
@@ -19,3 +19,18 @@ class Chat(models.Model):
 
     def __str__(self):
         return f"<{self.id}> {self.id_from} | {self.id_to} | {self.data} | {self.time} | {self.msg}"
+
+class Group_Chat(models.Model):
+    """
+    Модель записи групповых чатов
+    """
+    # Columns
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, help_text="Название")
+    uuid = models.IntegerField(help_text="Добавление участника")
+
+    class Meta:
+        verbose_name = "Group_Chat"
+
+    def __str__(self):
+        return f"<{self.id}> {self.title} | {self.uuid}"
