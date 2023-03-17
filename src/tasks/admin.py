653d6cbf22fa5503fca8a51e662604a06c9c2b4c
Stanislav <stas-chubar@yandex.ru>
2023-03-17 14:17:50 +0700

    reg models in admin.py

--- src/tasks/admin.py
+++ src/tasks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Tasks)
+admin.site.register(Task_status)
+admin.site.register(Task_visitors)
