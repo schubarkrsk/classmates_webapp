66f6bd95c8a590b6a945cf619c34a6c3060fc761
Stanislav <stas-chubar@yandex.ru>
2023-03-17 10:24:41 +0700

    registrated users model ina admin.py

--- src/timetable/admin.py
+++ src/timetable/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Teachers, Lessons
+from .models import Teachers, Lessons, Users
 
 admin.site.register(Teachers)
 admin.site.register(Lessons)
+admin.site.register(Users)
 # Register your models here.
