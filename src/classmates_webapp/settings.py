727c0e596db0ba65f798d56b67f8a2844078067f
Stanislav <stas-chubar@yandex.ru>
2023-03-17 14:24:31 +0700

    settings.py update

--- src/classmates_webapp/settings.py
+++ src/classmates_webapp/settings.py
@@ -107,9 +107,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
+"""
+Возможно этот коммит никто никогда не найдет, но я напишу
+Инн, я тебя люблю <3
+Стасяо Senior Python developer
+"""
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+7'
 
 USE_I18N = True
 
