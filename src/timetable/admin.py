f8eedd21dc4dea604e09e2942e78f2d48c234fe0
Stanislav <stas-chubar@yandex.ru>
2023-03-20 08:44:37 +0700

    db.sqlite3 update / backup

--- src/timetable/admin.py
+++ src/timetable/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Teachers, Lessons, Users, UserRoles
+from .models import Teachers, Lessons, Users, UserRoles, Timetable
 
 # Register your models here.
 admin.site.register(Teachers)
 admin.site.register(Lessons)
+admin.site.register(Timetable)
 admin.site.register(Users)
 admin.site.register(UserRoles)
