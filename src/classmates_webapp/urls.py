4ffe8e024046778f66ac97ee9d2f8595ccf59120
Shatan69 <Shpilberg.n@yandex.ru>
2023-04-03 23:05:40 +0700

    added tasks_events

--- src/classmates_webapp/urls.py
+++ src/classmates_webapp/urls.py
@@ -26,6 +26,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', timetableviews.index, name="home"), # TODO: Перенести в профильный URLs приложения
+    path('task', tasksviews.index, name="home"),
     path('egg', timetableviews.easter_egg, name="EasterEgg")
     # path('chat/<str:user>/', chatviews, name='chat'),
     # path('', include('chat.urls')),
--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Tasks
+from .models import Events
 # Create your views here.
+
+def index(request):
+    tasks = Tasks.objects.all()
+    event = Events.objects.all()
+    return render(request, 'tasks_events/tasks_events.html', {'tasks': tasks, 'event': event})
